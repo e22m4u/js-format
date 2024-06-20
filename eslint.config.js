5d0c253675bbfe9fbaa17e717a246fd7aa4fb7ce
e22m4u <e22m4u@yandex.ru>
2024-06-20 15:51:13 +0300

    chore: updates eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,25 +4,23 @@ import mochaPlugin from 'eslint-plugin-mocha';
 import chaiExpectPlugin from 'eslint-plugin-chai-expect';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
-export default [
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.es2021,
-        ...globals.mocha,
-      },
+export default [{
+  languageOptions: {
+    globals: {
+      ...globals.node,
+      ...globals.es2021,
+      ...globals.mocha,
     },
-    plugins: {
-      'mocha': mochaPlugin,
-      'chai-expect': chaiExpectPlugin,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...eslintConfigPrettier.rules,
-      ...mochaPlugin.configs.flat.recommended.rules,
-      ...chaiExpectPlugin.configs['recommended-flat'].rules,
-    },
-    files: ['src/**/*.js'],
   },
-];
+  plugins: {
+    'mocha': mochaPlugin,
+    'chai-expect': chaiExpectPlugin,
+  },
+  rules: {
+    ...js.configs.recommended.rules,
+    ...eslintConfigPrettier.rules,
+    ...mochaPlugin.configs.flat.recommended.rules,
+    ...chaiExpectPlugin.configs['recommended-flat'].rules,
+  },
+  files: ['src/**/*.js'],
+}];
