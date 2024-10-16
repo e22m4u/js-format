52d4eb3cca296bc34eab49fcaac2fd6d90ff2540
e22m4u <e22m4u@yandex.ru>
2024-10-16 17:53:16 +0300

    fix: esm error in cjs mode

--- build-cjs.js
+++ build-cjs.js
@@ -2,7 +2,7 @@ import * as esbuild from 'esbuild';
 
 await esbuild.build({
   entryPoints: ['src/index.js'],
-  outfile: 'dist/cjs/index.js',
+  outfile: 'dist/cjs/index.cjs',
   format: 'cjs',
   platform: 'node',
   target: ['node12'],
